4439a5be806147d4d9016845ce428d066e1d4dff
DongJoo <svra0945@gmail.com>
2021-12-05 22:55:10 +0900

    dd

--- src/programmers/level1/Keypad.java
+++ src/programmers/level1/Keypad.java
@@ -10,7 +10,7 @@ public class Keypad {
 	
 	
 	public static String solution(int[] numbers, String hand) {    
-		String answer = "";     
+		String answer = "";      
 		int [][] keyTable = { 	 {1,2,3}
 								,{4,5,6} 
 								,{7,8,9}
